ca02e20303d4382aaf9fb2d379de255af4742b52
Rajveer Mahida <edu.rajveersinh@gmail.com>
2023-11-13 14:32:50 +0530

    Add the Text Animation and fix Other Code issue

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// Shery.imageEffect("#back", {
-//     style: 5,
-//     debug: true,
-//     gooey:true
-//   });
+// Image Animtion
 
 Shery.imageEffect("#back", {
     style: 5,
@@ -29,3 +25,41 @@ Shery.imageEffect("#back", {
             "noise_scale": { "value": 12.15, "range": [0, 100] }
         }, gooey: true
 });
+
+
+
+// Text Animation 
+var elems = document.querySelectorAll(".elem");
+elems.forEach((elem) => {
+    var h1s = elem.querySelectorAll("h1");
+    var index = 0;
+    var animating = false;
+
+    document.querySelector("#main").addEventListener("click", function () {
+        if (!animating) {
+            animating = true;
+            gsap.to(h1s[index], {
+                top: "-=100%",
+                ease: Expo.easeInOut,
+                duration: 1,
+                onComplete: function () {
+                    gsap.set(this._targets[0], { top: "100%" });
+                    animating = false;
+                    console.log(index);
+                },
+            });
+
+            index === h1s.length - 1 ? (index = 0) : index++;
+
+            gsap.to(h1s[index], {
+                top: "-=100%",
+                ease: Expo.easeInOut,
+                duration: 1,
+            });
+        }
+    });
+});
+
+
+
+
